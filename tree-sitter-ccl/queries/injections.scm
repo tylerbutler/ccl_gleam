2a28c76de5245ceb478b7158d9e4b93b1de7dcee
Tyler Butler <tyler@tylerbutler.com>
2025-09-07 00:36:24 +0000

    move: relocate tree-sitter-ccl to tools-monorepo

--- tree-sitter-ccl/queries/injections.scm
+++
@@ -1,4 +0,0 @@
-; CCL self-injection for nested sections
-; This tells tree-sitter to re-parse nested section content as CCL
-((nested_content) @injection.content
- (#set! injection.language "ccl"))
